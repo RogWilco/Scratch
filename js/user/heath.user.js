3b146b64f9ec9773dc877ecfd976158a7cbc8be4
Nick Williams <git@nickawilliams.com>
2016-02-22 13:46:00 -0800

    Cleaned up header spacing some more.

--- js/user/heath.user.js
+++ js/user/heath.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name			Heath Ceramics - Overstock Improvements
-// @description		Adds color and shape previews to the overstock list for Heath ceramic tile.
-// @author			Nick Williams <rogwilco@gmail.com>
-// @namespace		http://github.com/rogwilco
+// @name            Heath Ceramics - Overstock Improvements
+// @description     Adds color and shape previews to the overstock list for Heath ceramic tile.
+// @author          Nick Williams <rogwilco@gmail.com>
+// @namespace       http://github.com/rogwilco
 //
-// @include			http://www.heathceramics.com/home/pages/tile-build/designing-with-heath-tile/overstock-tile
+// @include         http://www.heathceramics.com/home/pages/tile-build/designing-with-heath-tile/overstock-tile
 //
-// @require			http://code.jquery.com/jquery-1.8.0.min.js
+// @require         http://code.jquery.com/jquery-1.8.0.min.js
 //
-// @version			1.0.0
+// @version         1.0.0
 // ==/UserScript==
 
 (function($) {
